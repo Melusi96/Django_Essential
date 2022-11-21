cb9ba9c0166f6feea3aa365c40b0600ae70215ed
Melusi Gumbi <gmbmel003@myuct.ac.za>
2022-11-21 13:47:21 +0200

    Defined classes and removed functions in views
    Apparently it is much easier to work with classes instead of functions in views. Changed the way the urls connect with views, you have to call the classes "as_view()" and depending on what the class inherits, that will be the new display.

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Notes
+from django.views.generic import DetailView, ListView
 
 # Create your views here.
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+    # template_name = "notes/notes_detail.html" #I don't know why it works without including this line
