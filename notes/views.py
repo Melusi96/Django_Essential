63517958255feadbabc47cfa7b01bd8da538c114
Melusi Gumbi <gmbmel003@myuct.ac.za>
2022-11-24 02:33:36 +0200

    Adding a new note after ForeignKey
    Added a new note with the user logged in. Before calling the save method we set the commit to false so that it doesn't save to the database. Saving to the database without setting the user will cause an error stating that "You can't have a null user". The form_valid method is overridden so that the process of saving to the database can be interjected so that we can inject the user of the note being created.

--- notes/views.py
+++ notes/views.py
@@ -3,26 +3,36 @@ from django.http import Http404
 
 from .forms import NotesForm
 from .models import Notes
+from django.http.response import HttpResponseRedirect
 from django.views.generic import CreateView, UpdateView, DetailView, ListView
 from django.views.generic.edit import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class NotesCreateView(CreateView):
+class NotesCreateView(LoginRequiredMixin, CreateView):
     model = Notes
     success_url = '/smart/notes' #redirects the user to other created notes
     form_class = NotesForm
+    login_url = '/admin'
 
-class NotesUpdateView(UpdateView):
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesUpdateView(LoginRequiredMixin, UpdateView):
     model = Notes
     success_url = '/smart/notes' #redirects the user to other created notes
     form_class = NotesForm
+    login_url = '/admin'
 
-class NotesDeleteView(DeleteView):
+class NotesDeleteView(LoginRequiredMixin, DeleteView):
     model = Notes
     success_url = '/smart/notes'
     context_object_name = "note"
     template_name = 'notes/notes_delete.html'
+    login_url = '/admin'
 class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
@@ -33,7 +43,8 @@ class NotesListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return self.request.user.notes.all()
 
-class NotesDetailView(DetailView):
+class NotesDetailView(LoginRequiredMixin, DetailView):
     model = Notes
     context_object_name = "note"
     # template_name = "notes/notes_detail.html" #I don't know why it works without including this line
+    login_url = '/admin'
