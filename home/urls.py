9cbe4121248bf6fd3dfea55131eae42df4e5b63d
Melusi Gumbi <gmbmel003@myuct.ac.za>
2022-11-24 04:22:38 +0200

    Adding a signup page
    Created a new user besides the admin and when logging in they do not have notes due to overriding the get method under the signup class, so they won't access another user's information.

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     # path('authorized/', views.AuthorizedView.as_view()), #Our system don't really need this
+    path('signup/', views.SignupView.as_view(), name='signup'),
     path('login/', views.LoginInterfaceView.as_view(), name='login'),
     path('logout/', views.LogoutInterfaceView.as_view(), name='logout'),
 ]
