416fdb1e59f0eb86ac58a833463a7e20d83b34d9
Melusi Gumbi <gmbmel003@myuct.ac.za>
2022-11-24 03:33:49 +0200

    Added login and logout pages
    Created the login and logout page. With the login html you are creating a form that has a post.. You will need to import LoginView and LogoutView to be able to access the functionality of a login and of a logout.  Add a "LOGIN_REDIRECT_URL" in settings to set a path on where to go after logging in to the system.

--- notes/views.py
+++ notes/views.py
@@ -13,7 +13,7 @@ class NotesCreateView(LoginRequiredMixin, CreateView):
     model = Notes
     success_url = '/smart/notes' #redirects the user to other created notes
     form_class = NotesForm
-    login_url = '/admin'
+    login_url = 'login'
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
@@ -25,19 +25,19 @@ class NotesUpdateView(LoginRequiredMixin, UpdateView):
     model = Notes
     success_url = '/smart/notes' #redirects the user to other created notes
     form_class = NotesForm
-    login_url = '/admin'
+    login_url = 'login'
 
 class NotesDeleteView(LoginRequiredMixin, DeleteView):
     model = Notes
     success_url = '/smart/notes'
     context_object_name = "note"
     template_name = 'notes/notes_delete.html'
-    login_url = '/admin'
+    login_url = 'login'
 class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
-    login_url = '/admin'
+    login_url = 'login'
 
     '''Displaying only the logged user data'''
     def get_queryset(self):
@@ -47,4 +47,4 @@ class NotesDetailView(LoginRequiredMixin, DetailView):
     model = Notes
     context_object_name = "note"
     # template_name = "notes/notes_detail.html" #I don't know why it works without including this line
-    login_url = '/admin'
+    login_url = 'login'
